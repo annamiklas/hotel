24ebddedb62bab6638bf13c76f70a5b45f59a694
Anna Miklas <annamiklas7@gmail.com>
2024-07-07 10:20:32 +0200

    add jenkinsfile

--- src/main/java/com/hotel/Main.java
+++ src/main/java/com/hotel/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         final var manager = new InputManager();
         manager.run();
+        System.out.println("HELLO");
     }
 }
